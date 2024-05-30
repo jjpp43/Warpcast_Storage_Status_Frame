9696e3bda18b552e0c44cf7e8cbf014126ae1d18
jjpp43 <jparkdev63@gmail.com>
2024-05-31 02:04:07 +0900

    Test to see if cast action works

--- api/index.tsx
+++ api/index.tsx
@@ -4,6 +4,11 @@ import { serveStatic } from 'frog/serve-static'
 import { Box, Heading, Text, VStack, HStack, vars } from '../ui/ui.js'
 import { userDataStorage, userCastStorage, userLinkStorage, userReactionStorage } from './user_storage.js'
 
+import {
+  getFarcasterUserDetails,
+  validateFramesMessage,
+} from "@airstack/frog";
+
 // import { neynar } from 'frog/hubs'
 import { handle } from 'frog/vercel'
 
@@ -32,7 +37,7 @@ app.frame('/', (c) => {
     action: '/status',
     image: initFrame,
     intents: [
-      <Button value="myStats">Check My Stats</Button>,
+      <Button children='' value="myStats">Check My Stats</Button>,
     ],
   })
 })
@@ -46,14 +51,14 @@ app.frame('/status', (c) => {
 
   var determineSignalFunction = (val: number) => {
     if (val > 100) {
-      return <Text>&#x1F7E6;</Text>
+      return <Text children=''>&#x1F7E6;</Text>
     }
     else if (val > 75 && val <= 100) {
-      return <Text>&#x1F7E7;</Text>
+      return <Text children=''>&#x1F7E7;</Text>
     } else if (val <= 75 && val > 25) {
-      return <Text>&#x1F7E8;</Text>
+      return <Text children=''>&#x1F7E8;</Text>
     }
-    return <Text>&#x1F7E9;</Text>
+    return <Text children=''>&#x1F7E9;</Text>
 
   }
 
@@ -75,53 +80,85 @@ app.frame('/status', (c) => {
       >
         <VStack gap="12">
           <HStack>
-            <Heading weight='700'>FID: {userId}</Heading>
+            <Heading children='' weight='700'>FID: {userId}</Heading>
           </HStack>
           <Box height='8'></Box>
           <VStack gap="8">
             <HStack gap="4">
-              <Text weight='600'>Cast Storage</Text>
-              <Text>{castTextSignal} </Text>
-              <Text>
+              <Text children='' weight='600'>Cast Storage</Text>
+              <Text children=''>{castTextSignal} </Text>
+              <Text children=''>
                 : {userCastStorage.toFixed(1)}%
               </Text>
             </HStack>
             <HStack gap="4">
-              <Text>Reaction Storage</Text>
-              <Text>{reactionTextSignal} </Text>
-              <Text>
+              <Text children=''>Reaction Storage</Text>
+              <Text children=''>{reactionTextSignal} </Text>
+              <Text children=''>
                 : {userReactionStorage.toFixed(1)}%
               </Text>
             </HStack>
             <HStack gap="4">
-              <Text>Link Storage</Text>
-              <Text>{linkTextSignal} </Text>
-              <Text>
+              <Text children=''>Link Storage</Text>
+              <Text children=''>{linkTextSignal} </Text>
+              <Text children=''>
                 : {userLinkStorage.toFixed(1)}%
               </Text>
             </HStack>
             <HStack gap="4">
-              <Text>Total # of storages you have purchased : </Text>
-              <Text>{userDataStorage}</Text>
+              <Text children=''>Total # of storages you have purchased : </Text>
+              <Text children=''>{userDataStorage}</Text>
             </HStack>
           </VStack>
           <Box height='32'></Box>
           <HStack>
-            <Text size="12">Storage Indicator : [ &#x1F7E9; &#x1F7E9; &#x1F7E8; &#x1F7E7; ]</Text>
+            <Text children='' size="14" weight='700'>Storage Indicator : [ &#x1F7E9; &#x1F7E9; &#x1F7E8; &#x1F7E7; ]</Text>
           </HStack>
         </VStack>
 
       </Box>
     ),
     intents: [
-      <Button.AddCastAction action='/'>Share</Button.AddCastAction>,
-      status === 'response' && <Button.Reset>Reset</Button.Reset>,
+      <Button.AddCastAction children='' action='/gm'>Share</Button.AddCastAction>,
+      status === 'response' && <Button.Reset children=''>Reset</Button.Reset>,
     ],
   })
 
 })
 
-// Handle user data
+//To post a cast
+app.hono.post("/gm", async (c) => {
+  const body = await c.req.json();
+
+  // validate the POST body
+  const { isValid, message } = await validateFramesMessage(body);
+  const interactorFid = message?.data?.fid;
+  const castFid = message?.data.frameActionBody.castId?.fid as number;
+  if (isValid) {
+    // Check if trying to `GM` themselves
+    if (interactorFid === castFid) {
+      return c.json({ message: "Nice try." }, 400);
+    }
+
+    // Fetch user profile name
+    const { data, error } = await getFarcasterUserDetails({
+      fid: castFid,
+    });
+
+    if (error) {
+      return c.json({ message: "Error. Try Again." }, 500);
+    }
+
+    let message = `GM ${data?.profileName}!`;
+    if (message.length > 30) {
+      message = "GM!";
+    }
+
+    return c.json({ message });
+  } else {
+    return c.json({ message: "Unauthorized" }, 401);
+  }
+});
 
 
 // @ts-ignore
