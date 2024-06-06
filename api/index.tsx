028a11544bc46cf14c5108c49eafd70118879290
jjpp43 <jparkdev63@gmail.com>
2024-06-06 16:18:07 +0900

    Combine files userStorage.ts -> index.tsx

--- api/index.tsx
+++ api/index.tsx
@@ -1,7 +1,7 @@
 import { Button, Frog } from 'frog'
 import { devtools } from 'frog/dev'
 import { serveStatic } from '@hono/node-server/serve-static'
-import { userData, main } from './userStorage.js'
+//import { userData, main } from './userStorage.js'
 // import {
 //   getFarcasterUserDetails,
 //   validateFramesMessage,
@@ -10,6 +10,90 @@ import { userData, main } from './userStorage.js'
 import { handle } from 'frog/vercel'
 import { createSystem } from 'frog/ui'
 
+import axios from "axios";
+import { config } from "dotenv";
+
+config();
+
+// Define interfaces
+interface Limit {
+  storeType: string;
+  name: string;
+  limit: number;
+  used: number;
+  earliestTimestamp: number;
+  earliestHash: string;
+}
+
+interface LimitsResponse {
+  limits: Limit[];
+}
+
+//Export user storage data
+const userData = {
+  userCastStorage: "",
+  userLinkStorage: "",
+  userReactionStorage: "",
+  userDataStorage: 0,
+}
+
+// Main function to fetch and handle data
+const main = async (id: number) => {
+  const server = "https://hubs.airstack.xyz";
+
+  try {
+    const response = await axios.get<LimitsResponse>(`${server}/v1/storageLimitsByFid?fid=${id}`, {
+      headers: {
+        "Content-Type": "application/json",
+        "x-airstack-hubs": process.env.AIRSTACK_API_KEY,
+      },
+    });
+
+    // Check if response.data is defined
+    if (!response.data) {
+      console.error("Response data is undefined.");
+      return;
+    }
+
+    const tmp = response.data.limits;
+
+    tmp.forEach((i) => {
+      if (i["storeType"] === 'STORE_TYPE_CASTS') {
+        var t = i['used'] / i['limit'];
+        userData.userCastStorage = (parseFloat(t.toFixed(3)) * 100).toFixed(1);
+      }
+      if (i["storeType"] === 'STORE_TYPE_LINKS') {
+        var t = i['used'] / i['limit'];
+        userData.userLinkStorage = (parseFloat(t.toFixed(3)) * 100).toFixed(1);
+      }
+      if (i["storeType"] === 'STORE_TYPE_REACTIONS') {
+        var t = i['used'] / i['limit'];
+        userData.userReactionStorage = (parseFloat(t.toFixed(3)) * 100).toFixed(1);
+      }
+      if (i["storeType"] === 'STORE_TYPE_USER_DATA') {
+        userData.userDataStorage = i['used'] - 1;
+      }
+    })
+
+
+
+  } catch (e) {
+    // Enhanced error logging
+    if (axios.isAxiosError(e)) {
+      console.error("Axios error:", e.message);
+      if (e.response) {
+        console.error("Response data:", e.response.data);
+        console.error("Response status:", e.response.status);
+        console.error("Response headers:", e.response.headers);
+      }
+    } else {
+      console.error("Unexpected error:", e);
+    }
+  }
+}
+
+
+//-------------------------------------------
 const { Box, Heading, Text, VStack, HStack, vars } = createSystem({
   colors: {
     customBackground: '#453ECA',
